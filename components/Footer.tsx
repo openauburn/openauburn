fec3dffe8aacdec037061b505b3ae836e17bd529
Matthew Rogers <rogersmatt350@gmail.com>
2023-01-29 20:40:30 -0600

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,168 @@
+import { createStyles, Text, Container, ActionIcon, Group, UnstyledButton } from '@mantine/core';
+import { IconBrandTwitter, IconBrandInstagram, IconBrandLinkedin, IconBrandGithub, IconBrandDiscord} from '@tabler/icons';
+import OpenAuburnLogo from './OpenAuburnLogo';
+const useStyles = createStyles((theme) => ({
+  footer: {
+    paddingTop: theme.spacing.xl * 2,
+    paddingBottom: theme.spacing.xl * 2,
+    //backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    borderTop: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[2]
+    }`,
+  },
+
+  logo: {
+    maxWidth: 200,
+
+    [theme.fn.smallerThan('sm')]: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  },
+
+  description: {
+    marginTop: 5,
+
+    [theme.fn.smallerThan('sm')]: {
+      marginTop: theme.spacing.xs,
+      textAlign: 'center',
+    },
+  },
+
+  inner: {
+    display: 'flex',
+    justifyContent: 'space-between',
+
+    [theme.fn.smallerThan('sm')]: {
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+  },
+
+  groups: {
+    display: 'flex',
+    flexWrap: 'wrap',
+
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  wrapper: {
+    width: 160,
+  },
+
+  link: {
+    display: 'block',
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[1] : theme.colors.gray[6],
+    fontSize: theme.fontSizes.sm,
+    paddingTop: 3,
+    paddingBottom: 3,
+
+    '&:hover': {
+      textDecoration: 'underline',
+    },
+  },
+
+  title: {
+    fontSize: theme.fontSizes.lg,
+    fontWeight: 700,
+    marginBottom: theme.spacing.xs / 2,
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+  },
+
+  afterFooter: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: theme.spacing.xl,
+    paddingTop: theme.spacing.xl,
+    paddingBottom: theme.spacing.xl,
+    borderTop: `1px solid ${
+      theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]
+    }`,
+
+    [theme.fn.smallerThan('sm')]: {
+      flexDirection: 'column',
+    },
+  },
+
+  social: {
+    [theme.fn.smallerThan('sm')]: {
+      marginTop: theme.spacing.xs,
+    },
+  },
+}));
+
+interface FooterLinksProps {
+  data: {
+    title: string;
+    links: { label: string; link: string }[];
+  }[];
+}
+
+const Footer = ({ data }: FooterLinksProps) => {
+  const { classes } = useStyles();
+
+  const groups = data.map((group) => {
+    const links = group.links.map((link, index) => (
+      <Text
+        key={index}
+        className={classes.link}
+        component="a"
+        href={link.link}
+      >
+        {link.label}
+      </Text>
+    ));
+
+    return (
+      <div className={classes.wrapper} key={group.title}>
+        <Text className={classes.title}>{group.title}</Text>
+        {links}
+      </div>
+    );
+  });
+
+  return (
+    <footer className={classes.footer}>
+      <Container className={classes.inner}>
+        <div className={classes.logo}>
+          <UnstyledButton component="a" href="/">
+              <OpenAuburnLogo includeTitle/>
+          </UnstyledButton>
+          <Text size="xs" color="dimmed" className={classes.description}>
+          Create, research, and learn using Auburn University's largest public datasets.
+          </Text>
+        </div>
+        <div className={classes.groups}>{groups}</div>
+      </Container>
+      <Container className={classes.afterFooter}>
+        <Text color="dimmed" size="sm">
+          © 2023 Open Auburn. All rights reserved.
+        </Text>
+
+        <Group spacing={0} className={classes.social} position="right" noWrap>
+          <ActionIcon size="lg" component="a" href="https://github.com/openauburn" target="_blank" rel="noopener noreferrer">
+            <IconBrandGithub size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" component="a" href="https://www.linkedin.com/company/openauburn/" target="_blank" rel="noopener noreferrer">
+            <IconBrandLinkedin size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" component="a" href="https://discord.gg/pjabvqrReR" target="_blank" rel="noopener noreferrer">
+            <IconBrandDiscord size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" component="a" href="https://twitter.com/OpenAuburn" target="_blank" rel="noopener noreferrer">
+            <IconBrandTwitter size={18} stroke={1.5} />
+          </ActionIcon>
+          <ActionIcon size="lg" component="a" href="https://www.instagram.com/openauburn/" target="_blank" rel="noopener noreferrer">
+            <IconBrandInstagram size={18} stroke={1.5} />
+          </ActionIcon>
+        </Group>
+      </Container>
+    </footer>
+  );
+}
+
+export default Footer;
