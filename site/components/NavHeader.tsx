597aea20ae294443609f8e61006ea2bd43a807d3
Matthew Rogers <rogersmatt350@gmail.com>
2023-03-25 00:53:11 -0500

    Remove dead code

--- site/components/NavHeader.tsx
+++ site/components/NavHeader.tsx
@@ -185,10 +185,7 @@ const NavHeader = () => {
               </HoverCard.Dropdown>
             </HoverCard>
             <Link href="/showcase" className={classes.link}>
-            <Link href="/showcase" className={classes.link}>
               Showcase
-            </Link>
-            <Link href="/docs" className={classes.link}>
             </Link>
             <Link href="/docs" className={classes.link}>
               Documentation
@@ -196,52 +193,6 @@ const NavHeader = () => {
             <Link href="/about" className={classes.link}>
               About
             </Link>
-
-            {/* <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
-              <HoverCard.Target>
-                <a href="#" className={classes.link}>
-                  <Center inline>
-                    <Box component="span" mr={5}>
-                      Features
-                    </Box>
-                    <IconChevronDown size={16} color={theme.fn.primaryColor()} />
-                  </Center>
-                </a>
-              </HoverCard.Target>
-
-              <HoverCard.Dropdown sx={{ overflow: 'hidden' }}>
-                <Group position="apart" px="md">
-                  <Text weight={500}>Features</Text>
-                  <Anchor href="#" size="xs">
-                    View all
-                  </Anchor>
-                </Group>
-
-                <Divider
-                  my="sm"
-                  mx="-md"
-                  color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'}
-                />
-
-                <SimpleGrid cols={2} spacing={0}>
-                  {links}
-                </SimpleGrid>
-
-                <div className={classes.dropdownFooter}>
-                  <Group position="apart">
-                    <div>
-                      <Text weight={500} size="sm">
-                        Get started
-                      </Text>
-                      <Text size="xs" color="dimmed">
-                        Their food sources have decreased, and their numbers
-                      </Text>
-                    </div>
-                    <Button variant="default">Get started</Button>
-                  </Group>
-                </div>
-              </HoverCard.Dropdown>
-            </HoverCard> */}
 
           </Group>
 
