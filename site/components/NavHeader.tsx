b18612c21aae560f4662e1386eb662c253dd6db8
Matthew Rogers <rogersmatt350@gmail.com>
2023-03-25 00:50:22 -0500

    Update NavHeader.tsx

--- site/components/NavHeader.tsx
+++ site/components/NavHeader.tsx
@@ -184,71 +184,19 @@ const NavHeader = () => {
                 </div>
               </HoverCard.Dropdown>
             </HoverCard>
-            <a href="/showcase" className={classes.link}>
+            <Link href="/showcase" className={classes.link}>
               Showcase
-            </a>
-            <a href="/docs" className={classes.link}>
+            </Link>
+            <Link href="/docs" className={classes.link}>
               Documentation
-            </a>
-            <a href="/about" className={classes.link}>
+            </Link>
+            <Link href="/about" className={classes.link}>
               About
-            </a>
-
-            {/* <HoverCard width={600} position="bottom" radius="md" shadow="md" withinPortal>
-              <HoverCard.Target>
-                <a href="#" className={classes.link}>
-                  <Center inline>
-                    <Box component="span" mr={5}>
-                      Features
-                    </Box>
-                    <IconChevronDown size={16} color={theme.fn.primaryColor()} />
-                  </Center>
-                </a>
-              </HoverCard.Target>
-
-              <HoverCard.Dropdown sx={{ overflow: 'hidden' }}>
-                <Group position="apart" px="md">
-                  <Text weight={500}>Features</Text>
-                  <Anchor href="#" size="xs">
-                    View all
-                  </Anchor>
-                </Group>
-
-                <Divider
-                  my="sm"
-                  mx="-md"
-                  color={theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'}
-                />
-
-                <SimpleGrid cols={2} spacing={0}>
-                  {links}
-                </SimpleGrid>
-
-                <div className={classes.dropdownFooter}>
-                  <Group position="apart">
-                    <div>
-                      <Text weight={500} size="sm">
-                        Get started
-                      </Text>
-                      <Text size="xs" color="dimmed">
-                        Their food sources have decreased, and their numbers
-                      </Text>
-                    </div>
-                    <Button variant="default">Get started</Button>
-                  </Group>
-                </div>
-              </HoverCard.Dropdown>
-            </HoverCard> */}
-
+            </Link>
           </Group>
 
-          {/* <Group className={classes.hiddenMobile}>
-            <Group>
-              <Button variant="default">Log in</Button>
-              <Button>Sign up</Button>
-            </Group>
-            <LightDarkButton/>
-          </Group> */}
+
+          <LightDarkButton/>
 
           <Group className={classes.hiddenDesktop}>
             <div>
