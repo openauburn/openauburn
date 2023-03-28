469a0fffc6f27f852e15989e83aed1b7c32b95e8
Matthew Rogers <rogersmatt350@gmail.com>
2023-03-28 02:50:31 -0500

    Update fetchIcon.tsx

--- site/components/fetchIcon.tsx
+++ site/components/fetchIcon.tsx
@@ -1,6 +1,6 @@
 import * as Icons from "@tabler/icons";
 
-export default function FetchIcon(props: any) {
+export function FetchIcon(props: any) {
   let name = props.name === undefined ? "IconDatabase" : props.name;
   const IconComponent: any = Icons[name];
   return <IconComponent {...props} />;
