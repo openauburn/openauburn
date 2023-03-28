a2613999c79f69d2349fa48977dab7a9ae4c2eab
Matthew Rogers <rogersmatt350@gmail.com>
2023-03-28 12:16:08 -0500

    Add tags to dataset row item

--- site/pages/datasets/index.tsx
+++ site/pages/datasets/index.tsx
@@ -1,29 +1,36 @@
-import { Metadata } from "@/utils/types";
+import { Metadata, Tag } from "@/utils/types";
 import {
+  Badge,
+  Center,
   Container,
+  Divider,
   Flex,
   Grid,
   Group,
   Paper,
   Space,
+  Stack,
+  Switch,
   Text,
   ThemeIcon,
   Title,
   useMantineTheme,
 } from "@mantine/core";
-import React from "react";
+import React, { useState } from "react";
 import * as Icons from "@tabler/icons";
 import Link from "next/link";
 import { base } from "@/utils/api";
 import FetchIcon from "@/components/FetchIcon";
+import { IconClock } from "@tabler/icons";
 
 interface DatasetsProps {
   metadata: Array<Metadata>;
+  tags: Array<Tag>;
 }
 
 export default function Datasets(props: DatasetsProps) {
   const theme = useMantineTheme();
-
+  const [showDetails, setShowDetails] = useState<boolean>(false);
   return (
     <>
       <Container>
@@ -41,37 +48,129 @@ export default function Datasets(props: DatasetsProps) {
             </Grid.Col>
           </Grid>
         </div>
-
         <Space h={"xl"} />
 
-        {props.metadata.map((md: Metadata) => {
-          if (md.public) {
-            return (
-              <Link
-                href={`/datasets/${md.id}`}
-                key={Math.random() + Date.now()}
-                style={{ textDecoration: "none" }}
-              >
-                <Paper
-                  shadow="xs"
-                  p="md"
-                  withBorder
-                  style={{ marginBottom: 10 }}
-                >
-                  <Group spacing={"xs"}>
-                    <FetchIcon
-                      name={md.icon}
-                      size={22}
-                      color={theme.fn.primaryColor()}
-                    ></FetchIcon>
-                    <Title order={4}>{md.title}</Title>
-                  </Group>
-                  <Text>{md.summary}</Text>
-                </Paper>
-              </Link>
-            );
-          }
-        })}
+        <Space h={"sm"} />
+        <Grid>
+          <Grid.Col md={12} lg={2}>
+            <Title order={4}>Tags</Title>
+            {props.tags.map((tag: Tag) => {
+              return (
+                <Text ff={"monospace"} fz={"sm"}>
+                  {tag.title}
+                </Text>
+              );
+            })}
+          </Grid.Col>
+          <Grid.Col md={12} lg={10}>
+            <Container
+              sx={{ display: "flex", justifyContent: "space-between" }}
+            >
+              <Text
+                ff={"monospace"}
+                sx={{ fontSize: theme.fontSizes.sm }}
+              >{`${props.metadata.length} datasets found`}</Text>
+              <Group>
+                <Text>{`${showDetails ? "Hide" : "Show"} details`}</Text>
+                <Switch
+                  checked={showDetails}
+                  onChange={(event) =>
+                    setShowDetails(event.currentTarget.checked)
+                  }
+                ></Switch>
+              </Group>
+            </Container>
+            {props.metadata.map((md: Metadata) => {
+              if (md.public) {
+                return (
+                  <Paper
+                    shadow="xs"
+                    p="md"
+                    radius={"lg"}
+                    withBorder
+                    style={{ marginBottom: 10 }}
+                  >
+                    <Stack spacing={0}>
+                      <Link
+                        href={`/datasets/${md.id}`}
+                        key={Math.random() + Date.now()}
+                        style={{ textDecoration: "none" }}
+                      >
+                        <Group spacing={"xs"} sx={{ width: "100%" }}>
+                          <FetchIcon
+                            name={md.icon}
+                            size={22}
+                            color={theme.fn.primaryColor()}
+                          ></FetchIcon>
+                          <Title order={4} sx={{ color: "CaptionText" }}>
+                            {md.title}
+                          </Title>{" "}
+                        </Group>
+                      </Link>
+                      <Group spacing={3}>
+                        {(md.tags || []).map((tagID: number) => {
+                          let tagObj = props.tags.filter(
+                            (x) => x.id === tagID
+                          )[0];
+                          if (tagObj !== undefined) {
+                            return (
+                              <Paper>
+                                <Badge
+                                  pl={3}
+                                  sx={{
+                                    margin: "auto",
+                                  }}
+                                  ff={"monospace"}
+                                  size={"sm"}
+                                >
+                                  <Center>
+                                    {tagObj?.icon !== undefined ? (
+                                      <FetchIcon
+                                        size={13}
+                                        name={tagObj.icon}
+                                        style={{
+                                          marginRight: 5,
+                                          marginLeft: 5,
+                                        }}
+                                      />
+                                    ) : undefined}
+
+                                    {tagObj?.title}
+                                  </Center>
+                                </Badge>
+                              </Paper>
+                            );
+                          }
+                        })}
+                      </Group>
+                      <Text>{md.summary}</Text>
+                      <div style={{ display: showDetails ? "" : "none" }}>
+                        <Divider />
+                        <Group
+                          sx={{
+                            display: "flex",
+                            alignContent: "center",
+                          }}
+                        >
+                          <Group spacing={5}>
+                            <IconClock size={12}></IconClock>
+                            <Text
+                              ff={"monospace"}
+                              transform={"uppercase"}
+                              size={"xs"}
+                            >
+                              {md.updated_at.split("T")[0]}
+                            </Text>
+                          </Group>
+                        </Group>
+                      </div>
+                    </Stack>
+                  </Paper>
+                );
+              }
+            })}
+          </Grid.Col>
+        </Grid>
       </Container>
     </>
   );
@@ -82,8 +181,10 @@ export async function getServerSideProps() {
   const res = await fetch(base + "/metadata");
   const metadata = await res.json();
 
+  const res2 = await fetch(base + "/tags");
+  const tags = await res2.json();
   // return props
   return {
-    props: { metadata },
+    props: { metadata, tags },
   };
 }
