9763b194f1d7133171d120addaa2a504768ca94d
Matthew Rogers <rogersmatt350@gmail.com>
2023-03-30 17:40:57 -0500

    Create CustomBadge.tsx

---
+++ site/components/data_display/CustomBadge.tsx
@@ -0,0 +1,39 @@
+import { Badge, Center, Paper } from "@mantine/core";
+import React from "react";
+import FetchIcon from "../FetchIcon";
+
+interface CustomBadgeProps {
+  icon: string;
+  id: any;
+  title: string;
+}
+
+export default function CustomBadge(props: CustomBadgeProps) {
+  return (
+    <Paper key={props.id.toString()}>
+      <Badge
+        pl={3}
+        sx={{
+          margin: "auto",
+        }}
+        ff={"monospace"}
+        size={"sm"}
+      >
+        <Center>
+          {props.icon !== undefined ? (
+            <FetchIcon
+              size={13}
+              name={props.icon}
+              style={{
+                marginRight: 5,
+                marginLeft: 5,
+              }}
+            />
+          ) : undefined}
+
+          {props.title}
+        </Center>
+      </Badge>
+    </Paper>
+  );
+}
