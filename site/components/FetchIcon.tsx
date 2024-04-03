a355b5a201b75c83f2152920f9473f825e71dbf3
Matthew Rogers <alula_irl@proton.me>
2024-04-02 21:31:47 -0500

    commit neglect :(

--- site/components/FetchIcon.tsx
+++ site/components/FetchIcon.tsx
@@ -1,7 +1,7 @@
 import * as Icons from "@tabler/icons";
 
 export default function FetchIcon(props: any) {
-  let name = props.name === undefined ? "IconDatabase" : props.name;
+  let name = props.name === null ? "IconDatabase" : props.name;
   const IconComponent: any = Icons[name];
   return <IconComponent {...props} />;
 }
