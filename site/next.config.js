659ca95d36c530a90cb0e5922daa3f486dfc20f8
Matthew Rogers <alula_irl@proton.me>
2025-02-26 19:09:07 -0600

    disable image optimization

--- site/next.config.js
+++ site/next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
     //   { loader: '@next/font/google', options: { subsets: ['latin'] } },
     // ],
   },
+  images: {
+    unoptimized: true, // Disables Next.js automatic image optimization
+  },
 };
 
 module.exports = nextConfig;
